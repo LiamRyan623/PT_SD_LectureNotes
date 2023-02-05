8d2d46d9bc52ac183aee4c4ec0ed593a0e653888
unknown <LiamRyan623@outlook.com>
2023-02-05 15:05:51 -0500

    Updated Grid and Flex to remove nested repo.

--- Unit_2/operatior.js
+++ Unit_2/operatior.js
@@ -40,3 +40,42 @@ console.log( 2 <= 2, 1 <= 2, 5 <= 2) // True, True, False
 // And;&&
 console.log(2 < 10 && 2 > 1) // True
 
+// Or: || Looks for the first value that is true, and returns that.
+
+//* Logical Operators: determine the logic between variables or values
+// Ex with reassignment
+let a = 1; // 1
+
+let x = 3;
+let z = 5;
+console.log(x >= z);
+
+// Ex w/o reassignment
+let g = 100;
+console.log(g = 50); // 150
+console.log(g); // 100 
+
+// addition;
+r += 1; // r = r + 1
+
+let s = 50;
+console.log(s += 5); // s = 50 + 5
+console.log(s);
+
+// Subtraction
+r += 1; // r = r - 1
+
+// Multiplication
+r *=2 // r = r * 2
+
+// Division: /
+
+r /= 3 // r = r / 3
+
+// Remainder (modulus %)
+r %= 1; 
+
+// Exponential 
+r **=1; // r = r * r
+
+console.log(2 ** 3) // 8, ()
